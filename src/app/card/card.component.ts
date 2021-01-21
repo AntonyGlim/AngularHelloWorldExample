72254a0b13d56eafa3433014b91f6dece258c97e
Antony <antony.glim@gmail.com>
2021-01-21 22:18:05 +0300

    Work with vars

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -5,4 +5,10 @@ import {Component} from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {}
+export class CardComponent {
+  title = 'My title';
+  text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda enim est nihil possimus,' +
+    'praesentium quaerat quis repellendus saepe ut. Aut dolorum earum eveniet fugit illum magnam omnis possimus, quas.';
+  number = 1;
+  obj = {name: 'Victor', birthday: {year: 1990, month: 12, day: 21}};
+}
